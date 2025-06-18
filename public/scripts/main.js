b6e2ac773517311b519574c25c1f2cafcf97e6b1
Eryk Olliver <erykolliver@gmail.com>
2025-06-18 18:28:27 -0300

    projeto quase finalizado, faltando apenas a pagina sobre

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,33 @@
-document.querySelector(".el");
+/******/ (() => { // webpackBootstrap
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+var CaptureItens = document.querySelectorAll(".Item");
+var TeamContainer = document.querySelector("#TeamContainer");
+var container = document.querySelector("#container");
+var Teambtn = document.querySelector("#Teambtn");
+TeamContainer.ariaDisabled = true;
+CaptureItens.forEach(function (elem) {
+  elem.addEventListener('mouseover', function () {
+    elem.classList.add('hoverItem');
+  });
+  elem.addEventListener('mouseleave', function () {
+    setTimeout(function () {
+      elem.classList.remove('hoverItem');
+    }, 500);
+  });
+});
+Teambtn.addEventListener("click", function () {
+  container.classList.add('blur');
+  TeamContainer.classList.add('On');
+});
+if (TeamContainer.className === 'On') {
+  document.addEventListener('click', function (cl) {
+    if (cl.className != 'On') {
+      alert('dfsfs');
+    }
+  });
+}
+/******/ })()
+;
 //# sourceMappingURL=main.js.map
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,32 @@
-const El = document.querySelector('.el')
+const CaptureItens = document.querySelectorAll(".Item");
+const TeamContainer = document.querySelector("#TeamContainer");
+const container = document.querySelector("#container");
+const Teambtn = document.querySelector("#Teambtn")
+
+TeamContainer.ariaDisabled = true
+
+CaptureItens.forEach(elem => {
+    elem.addEventListener('mouseover', () => {
+        elem.classList.add('hoverItem');
+    })
+
+    elem.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            elem.classList.remove('hoverItem');
+        }, 500)
+    })
+})
+
+Teambtn.addEventListener("click", () => {
+    container.classList.add('blur')
+    TeamContainer.classList.add('On')
+})
+
+if (TeamContainer.className === 'On') {
+    document.addEventListener('click', (cl) => {
+        if (cl.className != 'On') {
+            alert('dfsfs')
+        }
+    })
+}
+
